f2cf6151579850cbcdedab5d19629bd18a9f978f
DaviSe2 <davidesenaeduardo@gmail.com>
2025-05-18 16:11:49 -0300

    ola mundo

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Olá, mundo!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  text: {
+    fontSize: 24,
+    color: '#333',
+  },
+});
